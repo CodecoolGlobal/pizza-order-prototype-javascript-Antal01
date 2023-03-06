ca258b104a37c3f1306da54c971d24215d724d20
Tihamer Antal <118159451+Antal01@users.noreply.github.com>
2023-03-06 23:02:44 +0100

    Server working

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,24 @@ const express = require('express')
 const app = express()
 const port = 9007;
 const fs = require('fs');
+const path = require("path");
+
+app.use(express.json());
 
 const rawData = JSON.parse(fs.readFileSync('./backend/data.json', "utf8"));
 const cards = rawData;
 
-app.get('/', (req,res) => {
+app.get("/", (req, res) => {
+    res.redirect(301, '/cards');
+  })
+
+app.get(["/cards","/cards/:id"], (req, res, next) => {
+    res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+  });
+
+app.use('/frontend', express.static('./frontend'))
+
+app.get('/api/cards', (req,res) => {
     res.send(cards)
 })
 
--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,6 @@
-const { response } = require("express");
 const rootElement = document.getElementById('root')
 
-fetch('http://127.0.0.1:9007/')
+fetch('http://127.0.0.1:9007/api/cards')
 .then((response) => response.json())
 .then((data) => display(data))
 
