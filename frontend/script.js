7bb3ebfc01ac3f19938cd1072bb1a2a0224ca12c
Borisz Bisits <118134911+BoriszBisits@users.noreply.github.com>
2023-03-08 13:16:31 +0100

    css

--- frontend/script.js
+++ frontend/script.js
@@ -2,20 +2,21 @@ const rootElement = document.getElementById('root')
 const menuElement = document.getElementById('menu')
 
 fetch('http://127.0.0.1:9007/api/cards')
-.then((response) => response.json())
-.then((data) => displayMain(data))
-let new_obj={
-    region: undefined,
-    type: undefined,
-    rarity: undefined,
+   .then((response) => response.json())
+   .then((data) => displayMain(data))
+let new_obj = {
+   region: undefined,
+   type: undefined,
+   rarity: undefined,
 };
 
 function displayMain(data) {
-      rootElement.replaceChildren();
-      menuElement.replaceChildren();
-    data.map(card => rootElement.insertAdjacentHTML('beforeend',
-     `<div>
-     Id:${card.id}
+   rootElement.replaceChildren();
+   menuElement.replaceChildren();
+   menuElement.insertAdjacentHTML("beforebegin", " <div id=mainTitle>Legends Of Runterra  </div>")
+   data.map(card => rootElement.insertAdjacentHTML('beforeend',
+      `<div>
+     
      Name: ${card.name}
      Type: ${card.type}
      Rarity: ${card.rarity}
@@ -26,34 +27,34 @@ function displayMain(data) {
 
      </div>`))
 
-//Region Selector
-let region=[];
+   //Region Selector
+   let region = [];
 
    for (let i = 0; i < data.length; i++) {
-     region.push(data[i].region)
+      region.push(data[i].region)
    }
    region = [...new Set(region)]
-    
+
    menuElement.insertAdjacentHTML('beforeend', '<select list=\'region\' id=\'region\'></select>');
    const regionDropdown = document.getElementById('region');
    regionDropdown.insertAdjacentHTML('beforeend', '<option id=blank>Please select region</option>');
 
    for (let i = 0; i < region.length; i++) {
-     regionDropdown.insertAdjacentHTML('beforeend', `<option id="region"> ${region[i]} </option>`);
+      regionDropdown.insertAdjacentHTML('beforeend', `<option id="region"> ${region[i]} </option>`);
    }
 
-///Type selctor
-let type=[];
+   ///Type selctor
+   let type = [];
 
    for (let i = 0; i < data.length; i++) {
-      if(data[i].type===''){
-         data[i].type="other"
+      if (data[i].type === '') {
+         data[i].type = "other"
       }
-   type.push(data[i].type)   
+      type.push(data[i].type)
    }
-   type=[...new Set(type)]
-    
-        
+   type = [...new Set(type)]
+
+
    menuElement.insertAdjacentHTML('beforeend', '<select list=\'type\' id=\'type\'></select>');
    const typeDropdown = document.getElementById('type');
    typeDropdown.insertAdjacentHTML('beforeend', '<option id=blank>Please select type</option>');
@@ -62,17 +63,17 @@ let type=[];
       typeDropdown.insertAdjacentHTML('beforeend', `<option id="type">${type[i]}</option>`);
    }
 
-///Rarity Selector
-let rarity=[];
+   ///Rarity Selector
+   let rarity = [];
 
    for (let i = 0; i < data.length; i++) {
-      if(data[i].rarity===''){
-       data[i].rarity="other"
+      if (data[i].rarity === '') {
+         data[i].rarity = "other"
       }
-   rarity.push(data[i].rarity)    
+      rarity.push(data[i].rarity)
    }
-   rarity=[...new Set(rarity)]
-   
+   rarity = [...new Set(rarity)]
+
    menuElement.insertAdjacentHTML('beforeend', '<select list=\'rarity\' id=\'rarity\'></select>');
    const rarityDropdown = document.getElementById('rarity');
    rarityDropdown.insertAdjacentHTML('beforeend', '<option id=blank>Please select rarity</option>');
@@ -80,14 +81,18 @@ let rarity=[];
    for (let i = 0; i < rarity.length; i++) {
       rarityDropdown.insertAdjacentHTML('beforeend', `<option id="rarity">${rarity[i]}</option>`);
    }
+
+   menuElement.insertAdjacentHTML("beforeend" , "<button id=resetbtn>Reset</button>")
 }
-function displayDeck(data){
-   let cardNumbersById=[]
+function displayDeck(data) {
+   let cardNumbersById = []
    rootElement.replaceChildren();
    menuElement.replaceChildren();
+
+   menuElement.insertAdjacentHTML("beforeend", " <div id=deckTitle> My Deck </div>")
    data.map(card => rootElement.insertAdjacentHTML('beforeend',
-  `<div>
-  Id:${card.id}
+      `<div>
+  
   Name: ${card.name}
   Type: ${card.type}
   Rarity: ${card.rarity}
@@ -97,48 +102,54 @@ function displayDeck(data){
   <button id="${card.id}">number</button>
   </div>`))
 }
-function sendObj(event) {   
-    
-   if(event.target.id === "type"){
+function sendObj(event) {
+
+   if (event.target.id === "type") {
       new_obj.type = event.target.value
    }
-    
-   if(event.target.id === "region"){
+
+   if (event.target.id === "region") {
       new_obj.region = event.target.value
    }
 
-   if(event.target.id === "rarity"){
+   if (event.target.id === "rarity") {
       new_obj.rarity = event.target.value
    }
-    console.log(new_obj);
+   console.log(new_obj);
 
-    const url = 'http://127.0.0.1:9007/api/cards';
-    fetch (url, {
+   const url = 'http://127.0.0.1:9007/api/cards';
+   fetch(url, {
       method: 'POST',
-      headers:{
-        'Content-Type': 'application/json',
+      headers: {
+         'Content-Type': 'application/json',
       },
       body: JSON.stringify(new_obj),
-    })
-    .then((response) => response.json())
-    .then((data) => displayMain(data))
+   })
+      .then((response) => response.json())
+      .then((data) => displayMain(data))
 }
-function addDeck(event){
+function addDeck(event) {
    fetch(`http://127.0.0.1:9007/api/cards/?addDeck=${event.target.id}`)
-   .then((response) => response.json())
-   .then((data) => displayMain(data))
+      .then((response) => response.json())
+      .then((data) => displayMain(data))
 }
 
-if(window.location.href === 'http://127.0.0.1:9007/cards/deck') {
-fetch('http://127.0.0.1:9007/api/cards/deck')
-.then((response) => response.json())
-.then((data) => displayDeck(data))
+if (window.location.href === 'http://127.0.0.1:9007/cards/deck') {
+   fetch('http://127.0.0.1:9007/api/cards/deck')
+      .then((response) => response.json())
+      .then((data) => displayDeck(data))
 }
 
 
-
-
-rootElement.addEventListener("click" , addDeck)
+function resetEvent(event)
+{
+   if(event.target.id==="resetbtn"){
+      displayMain(data)
+      console.log(event.target.id)
+   }
+}
+menuElement.addEventListener("click" , resetEvent)
+rootElement.addEventListener("click", addDeck)
 menuElement.addEventListener('change', sendObj);
 
 
